97c560ed38e07d0050c514330a623a7081433d56
DoYoung Park / 박도영 <cym7356@naver.com>
2022-11-28 21:48:48 +0900

    Add files via upload

---
+++ Javascript/크롬프로젝트/todoList/저장/todo.js
@@ -0,0 +1,19 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span); //li안의 span 생성
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  newTodo.value = "";
+  paintToDo(newTodo);
+}
+toDoForm.addEventListener("submit", handleToDoSubmit);
