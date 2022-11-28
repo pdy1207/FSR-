cf31a11c152539bb7ac6c9eac86e6f84af9386ff
DoYoung Park / 박도영 <cym7356@naver.com>
2022-11-28 23:02:11 +0900

    삭제 / 배열로서 JSON저장

---
+++ Javascript/크롬프로젝트/todoList/삭제 & 값넣기/todo.js
@@ -0,0 +1,37 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos)); //json 파일 저장하기
+}
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo); //버튼 클릭되는지
+  li.appendChild(span); //li안의 span 생성
+  li.appendChild(button); // li안의 버튼 생성
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  paintToDo(newTodo);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
