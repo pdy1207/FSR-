cbb94b0f56bf614eb8f1b1281eccba6462ee49a4
DoYoung Park <cym7356@naver.com>
2022-12-21 11:14:31 +0900

    Update tr 태그 & checkbox.js

--- Javascript/꿀팁/tr 태그 & checkbox.js
+++ Javascript/꿀팁/tr 태그 & checkbox.js
@@ -1,10 +1,16 @@
- //행 만 눌렀을때에 체크박스 기능 그 후 버튼 활성화 되게끔 완성하거나 둘 중 하나 지워야함.
-$("#table_customers").on('click', 'tr', function(e){
-    if( $(e.target).is('input:checkbox') ) return;
-    var chkbox = $(this).find('td:last-child :checkbox');
-    chkbox.p
-=============================================================================
+ 
 $("#tableId").on('click', 'tr', function(){
     var chkbox = $(this).find('td:first-child :checkbox');
     chkbox.prop('checked', !chkbox.prop('checked'));
 }
+=============================================================================
+                 
+하지만 체크박스를 직접 누를 시에는
+
+checkbox를 클릭을 해도 tr을 클릭한 상태로 인식을 하여서 checkbox가 눌려지지 않는다.
+                 
+$("#tableId").on('click', 'tr', function(e){
+    if( $(e.target).is('input:checkbox') ) return;
+    var chkbox = $(this).find('td:first-child :checkbox');
+    chkbox.prop('checked', !chkbox.prop('checked'));
+});
